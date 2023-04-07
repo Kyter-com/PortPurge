f0812b68b24cf7f04d8400e9dd03b63c4e6eca13
Nick Reisenauer <38516489+NickReisenauer@users.noreply.github.com>
2023-04-07 15:14:47 -0700

    feat: grab and print pid with new shell command

--- packages/rust/src/main.rs
+++ packages/rust/src/main.rs
@@ -2,6 +2,9 @@ use clap::Parser;
 use std::process::Command;
 use std::process::Stdio;
 
+mod utils;
+use crate::utils::trim_newline;
+
 #[derive(Parser, Debug)]
 #[command(version)]
 struct Args {
@@ -11,20 +14,31 @@ struct Args {
 }
 
 fn main() {
-    println!("Running portassassin");
+    println!("Running PortAssassin");
     let args = Args::parse();
 
     println!("Port to kill: {}", args.port);
 
-    let processes = Command::new("lsof")
-        .arg("-i")
-        .arg(format!(":{}", args.port))
+    let command = Command::new("lsof")
+        .arg(format!("-i:{}", args.port))
+        .arg("-t")
         .stdout(Stdio::piped())
         .spawn()
-        .expect("Failed to execute command");
+        .expect("Failed to execute command")
+        .wait_with_output()
+        .expect("Failed to wait for command");
 
-    println!(
-        "{:?}",
-        String::from_utf8_lossy(&processes.wait_with_output().unwrap().to_owned().stdout)
-    );
+    let mut pid_string = String::from_utf8(command.stdout).expect("Failed to get PID");
+
+    // Trim the trailing \n of the PID string
+    trim_newline(&mut pid_string);
+
+    if pid_string.is_empty() {
+        println!("No process running on port {}", args.port);
+        std::process::exit(0);
+    }
+
+    let pid: i32 = pid_string.parse().unwrap_or(0);
+
+    println!("Found process running with PID: {}", pid);
 }
