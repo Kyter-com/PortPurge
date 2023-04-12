e681dfeca350717ec7205ca7c5e10dabbf5ab9c1
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-04-12 16:49:45 -0700

    feat: add todo file

--- packages/rust/src/utils.rs
+++ packages/rust/src/utils.rs
@@ -1,3 +1,6 @@
+// use std::process::Command;
+// use std::process::Stdio;
+
 pub fn trim_newline(s: &mut String) {
     if s.ends_with('\n') {
         s.pop();
@@ -6,3 +9,25 @@ pub fn trim_newline(s: &mut String) {
         }
     }
 }
+
+// pub fn find_process_on_port(port: u32) -> Option<String> {
+//     let pid_command = Command::new("lsof")
+//         .arg(format!("-i:{}", port))
+//         .arg("-t")
+//         .stdout(Stdio::piped())
+//         .spawn()
+//         .expect("Failed to execute command")
+//         .wait_with_output()
+//         .expect("Failed to wait for command");
+
+//     let mut pid = String::from_utf8(pid_command.stdout).expect("Failed to get PID");
+
+//     // Trim the trailing \n of the PID string
+//     trim_newline(&mut pid);
+
+//     if pid.is_empty() {
+//         None
+//     } else {
+//         Some(pid)
+//     }
+// }
